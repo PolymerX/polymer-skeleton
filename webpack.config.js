c1ca65f5b94b2f1c26a69b9fd9b8ed07422a1772
LasaleFamine <info@godev.space>
2017-09-04 20:59:25 +0200

    Fixed index.html and webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const path = require('path');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const PostcssPolymerWebpackPlugin = require('./build_utils/postcss-polymer-webpack-plugin');
 
 const isBuild = process.env.NODE_ENV === 'build';
@@ -15,44 +14,21 @@ module.exports = {
   module: {
     rules: [{
       test: /\.html$/,
-      loaders: ['babel-loader', 'wc-loader', 'postcss-polymer-loader']
-    }, {
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
+      loaders: ['text-loader', 'postcss-html-loader']
     }]
   },
   plugins: [
-    new BrowserSyncPlugin(
-      // BrowserSync options
-      {
-        // Browse to http://localhost:3000/ during development
-        host: 'localhost',
-        port: 3000,
-        // Proxy the Webpack Dev Server endpoint
-        // (which should be serving on http://localhost:3100/)
-        // through BrowserSync
-        proxy: 'http://localhost:3100/'
-      },
-      {
-        // Prevent BrowserSync from reloading the page
-        // and let Webpack Dev Server take care of this
-        reload: false
-      }
-    ),
     new PostcssPolymerWebpackPlugin()
   ],
   devServer: {
     contentBase: path.resolve('src'),
     publicPath: '/',
     compress: true,
-    port: 3100,
+    port: 3000,
     watchOptions: {
       // Bower files and postcss ignored, reload when "style-module.html" is compiled(modified)
       // ignored: ['src/assets/bower/**/*', 'src/components/**/*.postcss'],
       poll: true
-    },
-    hot: true,
-    inline: true
+    }
   }
 };
