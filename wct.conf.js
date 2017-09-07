4df40e367dd7a04aa545c275a1af63d10a9a2796
LasaleFamine <info@godev.space>
2017-09-07 02:43:55 +0200

    Added conf wct and modified travis file

---
+++ wct.conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  verbose: true,
+  sauce: false,
+  suites: ['test/suites/*'],
+  plugins: {
+    sauce: {
+      disabled: true
+    },
+    local: {
+      remote: false,
+      browsers: ['chrome', 'firefox']
+    }
+  }
+};
