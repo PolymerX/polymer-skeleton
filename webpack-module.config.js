cb81264dba1ea59673ab4ff35a11a50c4a5a428e
LasaleFamine <info@godev.space>
2017-12-10 10:12:37 +0100

    chore(webpack-conf): aligned other conf to be functions

--- webpack-module.config.js
+++ webpack-module.config.js
@@ -4,33 +4,32 @@ const {resolve} = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const outputPath = resolve('dist');
-const isDev = process.argv.find(arg => arg.includes('webpack-dev-server'));
 
-const plugins = isDev ? [] : [
-  new CleanWebpackPlugin([outputPath], {verbose: true})
-];
-
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        // We need to transpile Polymer itself and other ES6 code
-        // exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [[
-              'env',
-              {
-                targets: {browsers: ['last 2 Chrome versions', 'Safari 10']},
-                debug: true
-              }
-            ]]
+module.exports = env => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          // We need to transpile Polymer itself and other ES6 code
+          // exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [[
+                'env',
+                {
+                  targets: {browsers: ['last 2 Chrome versions', 'Safari 10']},
+                  debug: true
+                }
+              ]]
+            }
           }
         }
-      }
+      ]
+    },
+    plugins: env.NODE_ENV === 'development' || process.env.NODE_ENV === 'development' ? [] : [
+      new CleanWebpackPlugin([outputPath], {verbose: true})
     ]
-  },
-  plugins
+  };
 };
--- webpack-nomodule.config.js
+++ webpack-nomodule.config.js
@@ -2,26 +2,28 @@
 
 const BROWSERS = ['> 1%', 'last 2 versions', 'Firefox ESR', 'not ie <= 11'];
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        // We need to transpile Polymer itself and other ES6 code
-        // exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [[
-              'env',
-              {
-                targets: {browsers: BROWSERS},
-                debug: true
-              }
-            ]]
+module.exports = () => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          // We need to transpile Polymer itself and other ES6 code
+          // exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [[
+                'env',
+                {
+                  targets: {browsers: BROWSERS},
+                  debug: true
+                }
+              ]]
+            }
           }
         }
-      }
-    ]
-  }
+      ]
+    }
+  };
 };
