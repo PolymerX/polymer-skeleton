3a7c445f189e8431bc0c2efd894898cc98313e5c
Manuel Martin <manuel.martin@gmail.com>
2018-02-08 23:38:48 +0100

    Update to polymer 3.0.0-pre.9

--- src/components/dumbs/sk-button/index.js
+++ src/components/dumbs/sk-button/index.js
@@ -1,13 +1,12 @@
 
-import {Element as PolymerElement} from '@polymer/polymer/polymer-element';
+import {Element as PolymerElement, html} from '@polymer/polymer/polymer-element';
 
 import css from './style.pcss';
 import template from './template.html';
 
 export default class SkButton extends PolymerElement {
   static get template() {
-    return `
-      <style>${css}</style> ${template}`;
+    return html([`<style>${css}</style> ${template}`]);
   }
 }
 
