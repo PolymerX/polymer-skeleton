80bf43784146afab4f1fcfa810b8d4032efdb3d3
LasaleFamine <info@godev.space>
2018-04-21 13:58:17 +0200

    hotfix(heroku): fix heroku script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,6 +4,6 @@ const {spawn} = require('child_process');
 const isHeroku = process.env.HEROKU;
 
 if (isHeroku) {
-  const child = spawn('yarn.cmd', ['build']);
+  const child = spawn('yarn', ['build']);
   child.on('exit', () => console.log('Finished.'));
 }
